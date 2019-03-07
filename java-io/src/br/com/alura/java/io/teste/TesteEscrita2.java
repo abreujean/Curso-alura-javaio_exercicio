372ee8f87e7b9956a1e1cd8a49bcc2f50ec3b48e
Jean Abreu <30299701+jeabreu@users.noreply.github.com>
2019-03-06 23:55:50 -0300

    Arquivo adicionado

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscrita2.java
@@ -0,0 +1,36 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class TesteEscrita2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		//Fluxo de Entrada com Arquivo
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+			
+		FileWriter fw = new FileWriter("lorem2.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		bw.newLine();
+		bw.newLine();
+		bw.write("Jean da Costa Abreu Teste e Tati Bonifacio Gomes Abreu");
+		
+		fw.close();
+	}
+
+}
